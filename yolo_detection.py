88c09b595633c01f93a4911465c0a40567a96df8
mo-ai-source <123095662+mo-ai-source@users.noreply.github.com>
2023-10-18 21:32:30 +0100

    Add files via upload

---
+++ yolo_detection.py
@@ -0,0 +1,39 @@
+import cv2
+import numpy as np
+
+def init_yolo():
+    net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+    layer_names = net.getLayerNames()
+    output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers().flatten()]
+    return net, output_layers
+
+def detect_basketballs(net, output_layers, img):
+    height, width, _ = img.shape
+    blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    net.setInput(blob)
+    outs = net.forward(output_layers)
+
+    class_ids = []
+    confidences = []
+    boxes = []
+
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+
+            if confidence > 0.5 and class_id == 32:  # Class ID for basketball is 32
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+
+    return boxes, confidences, class_ids
